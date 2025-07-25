592d06e2d9b3d6629f2f5908cea3aa7672bac90a
Katya Weicht <katya.weicht@gmail.com>
2025-07-25 13:41:56 -0600

    Serialize a collection

--- lib/serialize.py
+++ lib/serialize.py
@@ -24,9 +24,27 @@ class DogSchema(Schema):
     tail_wagging = fields.Boolean()
 
 # create model instance and schema instance
-
 dog = Dog(name="Snuggles", breed="Beagle", tail_wagging=True)
 
 dog_schema = DogSchema()
+#dump and dumps
 dog_dict = dog_schema.dump(dog)
 pprint(dog_dict)
+
+dog_json = dog_schema.dumps(dog)
+pprint(dog_json)
+
+#filtering
+dog_summary_only = DogSchema(only=("name","breed")).dumps(dog)
+pprint(dog_summary_only)
+
+dog_summary_excludes = DogSchema(exclude=("tail_wagging", )).dumps(dog)
+pprint(dog_summary_excludes)
+
+#Serialize a collection
+dogs = [Dog(name="Snuggles", breed="Beagle", tail_wagging=True),
+        Dog(name="Wags", breed = "Collie", tail_wagging=False)]
+dictionary_list = DogSchema(many=True).dump(dogs)
+pprint(dictionary_list)
+json_array = DogSchema(many=True).dumps(dogs)
+pprint(json_array)
